357dcee79bbd191dc512356187171ce26a4aafc5
thanhle1547 <thanhlm72@wru.vn>
2022-05-02 19:27:37 +0700

    [enum_name_map_arg_bloc] example (example_w_context): initial commit

---
+++ enum_name_map_arg_bloc/example_w_context/lib/screens/post_detail_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:saut_example_w_context/widget/favorite_button.dart';
+
+class PostDetailScreen extends StatelessWidget {
+  const PostDetailScreen({
+    Key? key,
+    required this.name,
+    required this.id,
+  }) : super(key: key);
+
+  final String name;
+  final int id;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Post detail'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(name),
+            FavoriteButton(id: id),
+          ],
+        ),
+      ),
+    );
+  }
+}
